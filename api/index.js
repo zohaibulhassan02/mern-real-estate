39177f671476960d3ddace36292b59dc9014e95a
Zohaib Ul Hassan <121067014+zohaibulhassan02@users.noreply.github.com>
2026-05-07 20:10:49 +0500

    DB Connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connection.on("connecting", () => console.log("Connecting..."));
+mongoose.connection.on("error", (err) => console.log("Mongoose error:", err));
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("MongoDB Connected Successfully!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
