10d91618852f43da02639576624113861bae5296
Zohaib Ul Hassan <121067014+zohaibulhassan02@users.noreply.github.com>
2026-05-18 14:39:26 +0500

    Middleware & Utils Functions Tested

--- api/index.js
+++ api/index.js
@@ -28,3 +28,14 @@ app.use(express.json());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+// Middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
