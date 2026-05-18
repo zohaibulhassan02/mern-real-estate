48b508a6f85491be17d37b0e8ef40a262c6a5dbc
Zohaib Ul Hassan <121067014+zohaibulhassan02@users.noreply.github.com>
2026-05-18 15:56:14 +0500

    Sign Up Functionality Completed

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": { target: "http://localhost:3000", secure: false },
+    },
+  },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
